5cd5ea690ffb73329bd0ded179a083b2ffb76fb6
Jatin Suri <surijatin@yahoo.com>
2024-06-08 17:13:11 -0400

    Added nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,20 @@ function App() {
   
   return (
     <>
+      <div className='menu'>
+        <div className="menu-item">About</div>
+        <div className="menu-item">Projects</div>
+        <div className="menu-item">Contact</div>
+      </div>
       <div className="hi-text">Hi.</div>
       {/* find a better way to do this */}
       <div className="test"></div>
       <div className='scroll-element'>
           <img className='animate-scroll' src={DownArrow} alt="Down Arrow" />
       </div>
+      <div className='content'>
+      </div>
+            
     </>
   )
 }
