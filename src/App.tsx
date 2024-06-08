dc29316c79b531de79329a0366a5f0239bd1842c
Jatin Suri <surijatin@yahoo.com>
2024-06-08 15:56:10 -0400

    Added Hi and scroll text

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useEffect } from 'react'
+import './App.css'
+import DownArrow from './icons/down-arrow.svg'
 
 function App() {
+  
   return (
-    <div>App</div>
+    <>
+      <section>
+        <span className="hi-text">Hi.</span>
+
+      </section>
+      {/* find a better way to do this */}
+      <div className="test"></div>
+      <div className='scroll-element'>
+        Scroll
+        <div>
+          <img className='animate-scroll' src={DownArrow} alt="Down Arrow" />
+        </div>
+      </div>
+    </>
   )
 }
 
