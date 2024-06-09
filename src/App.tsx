bf1b85085ffb2c350c0c3bde1dd2991849792b60
Jatin Suri <surijatin@yahoo.com>
2024-06-09 12:58:17 -0400

    Added scroll animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 // import React from 'react'
-// import { useEffect } from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 import './App.css'
+
 import DownArrow from './icons/down-arrow.svg'
 
 function App() {
-  
+
+  const [isVisible, setIsVisible] = useState(true);
+  const getOffset = (element: HTMLElement | null) => {
+    if (!element) return 0;
+    const rect = element.getBoundingClientRect();
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    return rect.top + scrollTop;
+  };
+  const heightToHIde = getOffset(document.querySelector('.content-text') as HTMLElement);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > heightToHIde) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+    });
+  }, []);
+
   return (
     <>
       <div className='menu'>
@@ -15,16 +35,18 @@ function App() {
       <div className="hi-text">Hi.</div>
       {/* find a better way to do this */}
       <div className='scroll-element'>
-          <img className='animate-scroll' src={DownArrow} alt="Down Arrow" />
-      </div>
+        
+        {<img className={`animate-scroll ${isVisible ? '' : 'hide'}`} src={DownArrow} alt="Down Arrow" />}
+      </div>       
       <div className='content'>
         <div className='content-text'>
-          <div>Hi I'm Jatin</div>
+          <div className='test'>Hi I'm Jatin</div>
         </div>
       </div>
-            
+
     </>
   )
 }
+
 
 export default App
