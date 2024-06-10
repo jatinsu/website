03d77f254db53f31dc050b92c09de5189b6fad8c
Jatin Suri <surijatin@yahoo.com>
2024-06-10 14:57:39 -0400

    made hi animation slightly better

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import { useEffect } from 'react'
 import { useState } from 'react'
 import './App.css'
 
+
 import DownArrow from './icons/down-arrow.svg'
 
 function App() {
-
+  // thanks to https://javascript.plainenglish.io/hiding-dom-elements-in-react-based-on-scrolling-d9a9ef1f1f5
   const [isVisible, setIsVisible] = useState(true);
   const getOffset = (element: HTMLElement | null) => {
     if (!element) return 0;
@@ -14,7 +15,7 @@ function App() {
     const scrollTop = window.scrollY || document.documentElement.scrollTop;
     return rect.top + scrollTop;
   };
-  const heightToHIde = getOffset(document.querySelector('.content-text') as HTMLElement);
+  const heightToHIde = getOffset(document.querySelector('.footer-text') as HTMLElement);
   useEffect(() => {
     window.addEventListener("scroll", () => {
       if (window.scrollY > heightToHIde) {
@@ -35,13 +36,16 @@ function App() {
       <div className="hi-text">Hi.</div>
       {/* find a better way to do this */}
       <div className='scroll-element'>
-        
+
         {<img className={`animate-scroll ${isVisible ? '' : 'hide'}`} src={DownArrow} alt="Down Arrow" />}
       </div>       
       <div className='content'>
         <div className='content-text'>
           <div className='test'>Hi I'm Jatin</div>
         </div>
+      </div>
+      <div className='footer'>
+        <div className='footer-text'>Made with ❤️ by Jatin</div>
       </div>
 
     </>
