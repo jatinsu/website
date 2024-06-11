4e9713a26a600bb3ea30ab876a4d56cc0400f45a
Jatin Suri <surijatin@yahoo.com>
2024-06-11 01:28:50 -0400

    Tried to fix mobile again + took out some classes

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -4,12 +4,12 @@ function Resume() {
   return (
    <> 
     <meta property="og:title" content="My Resume"/>
-    <meta property="og:description" content="My Portfolio Website"/>
+    <meta property="og:description" content="Testing"/>
     <meta property="og:image" content="/images/resume-embed.png"/>
     <meta name="twitter:card" content="summary_large_image"/>
     <title>Resume</title>
     <div className='pdf-container'>
-        <iframe className='pdf-embed'
+        <iframe
             title="Resume"
             // did this by publishing repo as a page and then using the link to the pdf for google drive
             src="https://jatinsu.github.io/website/images/Resume.pdf"
