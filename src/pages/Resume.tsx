bf672700357a113c25736b5087b8122611e12a85
Jatin Suri <surijatin@yahoo.com>
2024-06-11 00:55:04 -0400

    Testing a new way to embed pdf

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -12,8 +12,8 @@ function Resume() {
         <iframe
             title="Resume"
             // did this by publishing repo as a page and then using the link to the pdf for google drive
-            src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://jatinsu.github.io/website/images/Resume.pdf"
-            height="850"
+            src="https://jatinsu.github.io/website/images/Resume.pdf"
+            height="882"
             width="640"
         ></iframe>
     </div>
