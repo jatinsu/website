d3315ca96626c3f487b9950e02bceca134405150
Jatin Suri <surijatin@yahoo.com>
2024-06-10 23:50:04 -0400

    Added embeds (I think)

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -3,15 +3,19 @@ import './Resume.css'
 function Resume() {
   return (
    <> 
+    <meta property="og:title" content="My Resume"/>
+    <meta property="og:description" content="My Portfolio Website"/>
+    <meta property="og:image" content="/images/resume-embed.png"/>
+    <meta name="twitter:card" content="summary_large_image"/>
     <title>Resume</title>
     <div className='pdf-container'>
-    <iframe
-    title="Resume"
-    // did this by publishing repo as a page and then using the link to the pdf for google drive
-    src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://jatinsu.github.io/website/images/Resume.pdf"
-    height="850"
-    width="640"
-></iframe>
+        <iframe
+            title="Resume"
+            // did this by publishing repo as a page and then using the link to the pdf for google drive
+            src="https://drive.google.com/viewerng/viewer?embedded=true&url=https://jatinsu.github.io/website/images/Resume.pdf"
+            height="850"
+            width="640"
+        ></iframe>
     </div>
    
    </>
