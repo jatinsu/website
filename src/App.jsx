9041f43b81126cb7643678dd0c5770b8f015cf59
Jatin Suri <surijatin@yahoo.com>
2025-01-15 18:30:18 -0500

    Added dev image and helped mobile look better

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,17 @@ function App() {
 
   return (
     <>
-      <div className='intro'>Hey, my name is</div>
-      <div className='name'>Jatin Suri</div>
-      <div className='description'>I'm a Software Engineer based out of Maryland</div>
+      <div className='container'>
+        <div className='text-container'>
+          <div className='intro'>Hey, my name is</div>
+          <div className='name'>Jatin Suri</div>
+          <div className='description'>I'm a Software Engineer based out of Maryland</div>
+        </div>
+
+        <div className='image-container'>
+          <img className='dev_image' src="src/assets/dev-image.png" alt="" />
+        </div>
+      </div>
     </>
   )
 }
