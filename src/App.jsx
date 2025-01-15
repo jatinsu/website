e5e37e69cf2e905f7b8f404f15387b0b08bb7c51
Jatin Suri <surijatin@yahoo.com>
2025-01-15 02:53:46 -0500

    Uploading new version of website

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='intro'>Hey, my name is</div>
+      <div className='name'>Jatin Suri</div>
+      <div className='description'>I'm a Software Engineer based out of Maryland</div>
+    </>
+  )
+}
+
+export default App
