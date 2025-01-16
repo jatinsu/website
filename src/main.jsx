25ab726570b48bdd63d0d30530e7b9e9268d81a9
Jatin Suri <surijatin@yahoo.com>
2025-01-16 13:05:20 -0500

    Added Routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Home from './pages/Home'
+import Resume from './pages/Resume'
+import { BrowserRouter as Router, Route, BrowserRouter, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/resume" element={<Resume />} />
+    </Routes>
+  </BrowserRouter>
 )
