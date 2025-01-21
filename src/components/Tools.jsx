ea8d5c9b9240cdf0e491a664e0fe18bf6e736d7b
Jatin Suri <surijatin@yahoo.com>
2025-01-21 02:49:29 -0500

    Added more tools

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -3,6 +3,12 @@ import cplusIcon from '../assets/icons/cplus.svg'
 import pythonIcon from '../assets/icons/python.svg'
 import reactIcon from '../assets/icons/react.svg'
 import htmlIcon from '../assets/icons/html.svg'
+import goIcon from '../assets/icons/go.svg'
+import vrlIcon from '../assets/icons/vrl.svg'
+import javascriptIcon from '../assets/icons/javascript.svg'
+import typescriptIcon from '../assets/icons/typescript.svg'
+import cssIcon from '../assets/icons/css.svg'
+import autdeskIcon from '../assets/icons/autodesk.svg'
 
 
 function Tools(){
@@ -11,6 +17,12 @@ function Tools(){
         { name: 'Python', icon: pythonIcon },
         { name: 'React', icon: reactIcon },
         { name: 'HTML', icon: htmlIcon },
+        { name: 'Go', icon: goIcon },
+        { name: 'VRL', icon: vrlIcon },
+        { name: 'JavaScript', icon: javascriptIcon },
+        { name: 'TypeScript', icon: typescriptIcon },
+        { name: 'CSS', icon: cssIcon },
+        { name: 'Autodesk', icon: autdeskIcon }
     ];
 
     return(
