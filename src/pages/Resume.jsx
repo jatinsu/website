303d85a391a5079c4ba3d11833ba68cceb34d54a
Jatin Suri <surijatin@yahoo.com>
2025-01-26 02:35:45 -0500

    added navbar to resume and fixed navbar on mobile

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,17 +1,19 @@
 import './Resume.css'
 import ResumePDF from '../assets/Resume.pdf'
+import Navbar from '../components/Navbar'
 
 function Resume() {
   return (
-    <div className='pdf-container'>
-        <iframe
-            title="Resume"
-            src={ResumePDF}
-        ></iframe>
-    </div>
+    <>
+      <Navbar />
+      <div className='pdf-container'>
+          <iframe
+              title="Resume"
+              src={ResumePDF}
+          ></iframe>
+      </div>
+    </>
   )
 }
-
-
 
 export default Resume
